03045a7ddb822e179cefffc688138b79cb597cc6
idrmiller <idrmiller360@gmail.com>
2015-04-11 22:07:59 -0400

    Complete Error Handling for no internet connection Add the refresh button, to update the current temp, time, humidity, rain

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var perciptationLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
     
     
     private let apiKey = "7bb2c2a909c20c82f3bb31b1f2873ec8"
@@ -25,6 +27,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        refreshActivityIndicator.hidden = true
+        getCurrentWeatherData()
+       
+    }
+    
+    // We created this function, beacsue the download method .viewDidLoad code was getting to large.
+    // This is also considered out network code
+    func getCurrentWeatherData() -> Void {
         
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
         let forcastURL = NSURL(string: "38.897193,-77.025013", relativeToURL: baseURL)
@@ -45,7 +55,7 @@ class ViewController: UIViewController {
                 // This bit of code is to help convert our dataObject to JSOn to make it readable.
                 let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
                 
-               let currentWeather = Current(weatherDictionary: weatherDictionary)
+                let currentWeather = Current(weatherDictionary: weatherDictionary)
                 
                 // Allows us to ensure proper queing the code for best performance.
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
@@ -55,15 +65,51 @@ class ViewController: UIViewController {
                     self.humidityLabel.text = "\(currentWeather.humidity)"
                     self.perciptationLabel.text = "\(currentWeather.precipProbability)"
                     self.summaryLabel.text = "\(currentWeather.summary)"
+                    
+                    // Stop refresh animation
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
                 })
                 
+            } else {
+                
+                let networkIssueController = UIAlertController(title: "Error", message: "Unable to load data. Connectivity error!", preferredStyle: .Alert)
+                
+                let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                networkIssueController.addAction(okButton)
+                
+                let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+                networkIssueController.addAction(cancelButton)
+                
+                
+                self.presentViewController(networkIssueController, animated: true, completion: nil)
+                
+                
+                //  After error this gets us back into the main que so our refresh action can complete.
+                //  Need to look up this method a bit more.
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    // Stop refresh animation
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
+                })
+                
+                
             }
-            
-            
             
         })
         
         downloadTask.resume()
+    }
+    
+    
+    @IBAction func refresh() {
+        getCurrentWeatherData()
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
